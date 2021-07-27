3d63b3705241558dc0f1f651045a773467014407
wangpu <wangpu@tubao.onaliyun.com>
2021-07-27 18:47:11 +0800

    栅栏函数

---
+++ MyLibs/锁/栅栏/AFBarrierViewController.h
@@ -0,0 +1,16 @@
+//
+//  AFBarrierViewController.h
+//  MyLibs
+//
+//  Created by Joey on 2021/7/27.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AFBarrierViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
